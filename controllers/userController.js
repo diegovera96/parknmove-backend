885280705e85a295d290f9d6c67a36e8904c05e1
Diego_Vera <diego.vera@alumnos.ucn.cl>
2023-10-19 23:46:10 -0300

    fix: Autentificación en frontend y backend por separado

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,16 +7,23 @@ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 // Controlador para el registro de usuarios
 exports.register = async (req, res) => {
     const { name, lastname, email, password, priority } = req.body;
+    const errors = [];
     try {
         //Validaciones
         if(name === "" || lastname === ""){
-            return res.status(400).json({ error: "Parámetros inválidos." });
+            //return res.status(400).json({ error: "Parámetros inválidos." });
+            errors.push("Parámetros inválidos.");
         }
         if (password.length < 8) {
-            return res.status(400).json({ error: "Contraseña inválida. (min 8)" });
+            //return res.status(400).json({ error: "Contraseña inválida. (min 8)" });
+            errors.push("Contraseña inválida. (min 8).");
         }
         if(!emailRegex.test(email)){
-            return res.status(400).json({ error: "Correo electrónico inválido." });
+            //return res.status(400).json({ error: "Correo electrónico inválido." });
+            errors.push("Correo electrónico inválido.");
+        }
+        if (errors.length > 0) {
+            return res.status(400).json({ errors });
         }
 
         //Verificar si el usuario ya existe
@@ -25,7 +32,11 @@ exports.register = async (req, res) => {
         });
     
         if (existingUser) {
-            return res.status(400).json({ error: "Ya hay un usuario registrado con este correo." });
+            //return res.status(400).json({ error: "Ya hay un usuario registrado con este correo." });
+            errors.push("Correo ya registrado.");
+        }
+        if (errors.length > 0) {
+            return res.status(400).json({ errors });
         }
 
         //Hash de la contraseña
@@ -51,22 +62,26 @@ exports.register = async (req, res) => {
         res.status(201).json({ message: 'Usuario creado exitosamente', user, token });
 
     } catch (error) {
-        console.error(error);
+        //console.error(error);
         res.status(500).json({ message: 'Error en el servidor' });
     }
 };
 
 exports.login = async (req, res) => {
     const { email, password } = req.body;
+    const errors = [];
     try {
-
         //Verificar si el usuario ya existe
         const existingUser = await User.findOne({
             where: { email },
         });
     
         if (!existingUser) {
-            return res.status(400).json({ error: "No hay un usuario registrado con este correo." });
+            //return res.status(400).json({ error: "No hay un usuario registrado con este correo." });
+            errors.push("El usuario no existe.");
+        }
+        if (errors.length > 0) {
+            return res.status(400).json({ errors });
         }
 
         //Hash de la contraseña ingresada
@@ -79,14 +94,18 @@ exports.login = async (req, res) => {
 
         //Verificar si la contraseña es correcta
         if (passwordHash === existingUser.password) {
-        const token = existingUser.generateAuthToken();
-        return res.status(200).json({ message: 'Inicio de sesión exitoso', existingUser, token });
+            const token = existingUser.generateAuthToken();
+            return res.status(200).json({ message: 'Inicio de sesión exitoso', existingUser, token });
+        }else{
+            //return res.status(401).json({ error: 'Credenciales inválidas' });
+            errors.push("Error al iniciar sesión.");
+        }
+        if (errors.length > 0) {
+            return res.status(400).json({ errors });
         }
 
-        return res.status(401).json({ error: 'Credenciales inválidas' });
-
     } catch (error) {
-        console.error(error);
+        //console.error(error);
         res.status(500).json({ message: 'Error en el servidor' });
     }
 };
