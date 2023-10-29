9693048accf23bbdb2e1c4641c57e11bedc413b6
Joaquin Pinto <joaquin-pinto@alumnos.ucn.cl>
2023-10-29 17:00:17 -0300

    ReservaV1

--- src/controllers/ReservationController.js
+++ src/controllers/ReservationController.js
@@ -4,15 +4,17 @@ const Reservation = db.parking_user;
 // Función para crear una reserva
 const createReservation = async (req, res) => {
   try {
-    const { user_id, parking_id, entry_time } = req.body;
-    
+    const { user_id, parking_id, total_price, entry_time, exit_time, extra_fee } = req.body;
+    res.status(500).json({ error: "Error al crear la reserva" + JSON.stringify(req.body) });
     // Crea una nueva reserva en la base de datos
     const reservation = await Reservation.create({
       user_id,
       parking_id,
+      total_price,
       entry_time,
+      exit_time,
+      extra_fee,
     });
-    
     // Devuelve la reserva creada en la respuesta
     res.status(201).json(reservation);
   } catch (error) {
